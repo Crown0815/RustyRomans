1986f5308f5ddfba415e9d61442aaea4d1f296d8
Felix Kröner <felix.kroener@gmx.de>
2022-07-26 06:45:01 +0200

    Finish first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 macro_rules! reduce {
-    ($number: expr, $divisor: expr, $character: expr, $result: ident) => {
+    ($number: ident, $divisor: expr, $character: expr, $result: ident) => {
         if $number / $divisor > 0 {
             for _ in 0..$number / $divisor {
-                $result.push($character);
+                $result.push_str($character);
             }
-            return $result;
+            $number = $number - ($number / $divisor) * $divisor;
         }
     }
 }
 
+#[allow(unused_assignments)]
 pub fn numeral_for(number: i32) -> String{
     let mut result = String::default();
+    let mut rest = number;
 
-    reduce!(number, 1000,'M', result);
-    reduce!(number, 100,'C', result);
-    reduce!(number, 10,'X', result);
-    reduce!(number, 1,'I', result);
+    reduce!(rest, 1000,"M", result);
+    reduce!(rest, 900,"CM", result);
+    reduce!(rest, 500,"D", result);
+    reduce!(rest, 400,"CD", result);
+    reduce!(rest, 100,"C", result);
+    reduce!(rest, 90,"XC", result);
+    reduce!(rest, 50,"L", result);
+    reduce!(rest, 40,"XL", result);
+    reduce!(rest, 10,"X", result);
+    reduce!(rest, 9,"IX", result);
+    reduce!(rest, 5,"V", result);
+    reduce!(rest, 4,"IV", result);
+    reduce!(rest, 1,"I", result);
 
-    return String::default();
+    return result;
 }
--- tests/roman_spec.rs
+++ tests/roman_spec.rs
@@ -1,50 +1,79 @@
 #[cfg(test)]
 mod tests {
+    use test_case::test_case;
 
-    #[test]
-    fn roman_one_is_i() {
-        assert_eq!(roman_rust::numeral_for(1), "I");
+    #[test_case(1, "I")]
+    #[test_case(10, "X")]
+    #[test_case(100, "C")]
+    #[test_case(1000, "M")]
+    fn roman_power_of_ten_is_single_numeral(number: i32, numeral: &str) {
+        assert_eq!(roman_rust::numeral_for(number), numeral);
     }
 
-    #[test]
-    fn roman_ten_is_x() {
-        assert_eq!(roman_rust::numeral_for(10), "X");
+    #[test_case(1, "I")]
+    #[test_case(10, "X")]
+    #[test_case(100, "C")]
+    #[test_case(1000, "M")]
+    fn roman_twice_power_of_ten_is_twice_numeral(number: i32, numeral: &str) {
+        assert_eq!(roman_rust::numeral_for(2*number), format!("{0}{0}", numeral));
     }
 
-    #[test]
-    fn roman_hundred_is_c() {
-        assert_eq!(roman_rust::numeral_for(100), "C");
-    }
-
-    #[test]
-    fn roman_thousand_is_m() {
-        assert_eq!(roman_rust::numeral_for(1000), "M");
-    }
-
-    #[test]
-    fn roman_two_is_twice_i() {
-        assert_eq!(roman_rust::numeral_for(2), "II");
-    }
-
-    #[test]
-    fn roman_three_is_thrice_i() {
-        assert_eq!(roman_rust::numeral_for(3), "III");
-    }
-
-    #[test]
-    fn roman_twenty_is_twice_x() {
-        assert_eq!(roman_rust::numeral_for(20), "XX");
-    }
-
-    #[test]
-    fn roman_200_is_twice_c() {
-        assert_eq!(roman_rust::numeral_for(200), "CC");
-    }
-
-    #[test]
-    fn roman_2000_is_twice_m() {
-        assert_eq!(roman_rust::numeral_for(2000), "MM");
+    #[test_case(1, "I")]
+    #[test_case(10, "X")]
+    #[test_case(100, "C")]
+    #[test_case(1000, "M")]
+    fn roman_thrice_power_of_ten_is_thrice_numeral(number: i32, numeral: &str) {
+        assert_eq!(roman_rust::numeral_for(3*number), format!("{0}{0}{0}", numeral));
     }
 
 
+    #[test_case(1, "V")]
+    #[test_case(10, "L")]
+    #[test_case(100, "D")]
+    //five times 1000 is out of roman numerals range (1 - 3999)
+    fn roman_five_times_power_of_ten_is_specific_numeral(number: i32, numeral: &str) {
+        assert_eq!(roman_rust::numeral_for(5*number), numeral);
+    }
+
+    #[test_case(1, "I", "V")]
+    #[test_case(10, "X", "L")]
+    #[test_case(100, "C", "D")]
+    fn roman_six_times_power_of_ten_is_specific_numeral_for_five_times_and_once(number: i32, once: &str, five_times: &str) {
+        assert_eq!(roman_rust::numeral_for(6*number), format!("{0}{1}", five_times, once));
+    }
+
+    #[test_case(1, "I", "V")]
+    #[test_case(10, "X", "L")]
+    #[test_case(100, "C", "D")]
+    fn roman_seven_times_power_of_ten_is_specific_numeral_for_five_times_and_twice(number: i32, once: &str, five_times: &str) {
+        assert_eq!(roman_rust::numeral_for(7*number), format!("{0}{1}{1}", five_times, once));
+    }
+
+    #[test_case(1, "I", "V")]
+    #[test_case(10, "X", "L")]
+    #[test_case(100, "C", "D")]
+    fn roman_eight_times_power_of_ten_is_specific_numeral_for_five_times_and_thrice(number: i32, once: &str, five_times: &str) {
+        assert_eq!(roman_rust::numeral_for(8*number), format!("{0}{1}{1}{1}", five_times, once));
+    }
+
+    #[test_case(1, "I", "V")]
+    #[test_case(10, "X", "L")]
+    #[test_case(100, "C", "D")]
+    fn roman_four_times_power_of_ten_is_specific_numeral_for_once_and_five(number: i32, once: &str, five_times: &str) {
+        assert_eq!(roman_rust::numeral_for(4*number), format!("{0}{1}", once, five_times));
+    }
+
+    #[test_case(1, "I", "X")]
+    #[test_case(10, "X", "C")]
+    #[test_case(100, "C", "M")]
+    fn roman_nine_times_power_of_ten_is_specific_numeral_for_once_and_next_power_of_ten(number: i32, once: &str, ten_times: &str) {
+        assert_eq!(roman_rust::numeral_for(9*number), format!("{0}{1}", once, ten_times));
+    }
+
+    #[test_case(1111, "MCXI")]
+    #[test_case(999, "CMXCIX")]
+    #[test_case(3999, "MMMCMXCIX")]
+    fn other_numbers_are_concatenated_versions_of_the_previous_cases(number: i32, numeral: &str) {
+        assert_eq!(roman_rust::numeral_for(number), numeral);
+    }
 }
